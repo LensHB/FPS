ff60fb6c86e1ce9f5b666cc75ca83bbb8fe56bdd
lensHB <hzylens@gmail.com>
2019-03-26 20:18:23 +0800

    Initial commit

---
+++ Source/MyProject4/Updown.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Updown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT4_API UUpdown : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
